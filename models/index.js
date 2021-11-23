80f6e780ac0d15408a9a8d395f836e19cbbc8daf
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-22 20:36:00 -0500

    updated models index.js to export models

--- models/index.js
+++ models/index.js
@@ -4,6 +4,11 @@ const Workout = require("./Workout");
 const Exercise = require("./Exercise");
 const MealPlan = require("./MealPlan");
 const Recipe = require("./Recipe");
+const User = require("./User");
+
+//user has one workout plan
+
+//user has one meal plan
 
 //fitnessplan has many workouts
 
@@ -16,3 +21,5 @@ const Recipe = require("./Recipe");
 //meal plan has many recipes
 
 //recipes has many ingridients (might not need an ingridient model depeding on how we set up an api that gives us recipes)
+
+module.exports = { FitnessPlan, Workout, Exercise, MealPlan, Recipe, User };
