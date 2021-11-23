b1dded32ac1d430ba93743b539faab6a9119f4cb
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-22 19:35:11 -0500

    built out version 1 of fitnessplan model

--- models/FitnessPlan.js
+++ models/FitnessPlan.js
@@ -1,3 +1,32 @@
 const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class FitnessPlan extends Model {}
+
+FitnessPlan.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    workouts: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "fitnessplan",
+  }
+);
 
 module.exports = FitnessPlan;
