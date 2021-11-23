a8fd2a260227b99417ff47073cc61cc419a0c098
Hugo Yanez <hugodavidyanez@gmail.com>
2021-11-23 15:05:22 -0500

    updated server.js and edited mealplan.js and recipe.js

--- controllers/api/fitness-plan-routes.js
+++ controllers/api/fitness-plan-routes.js
@@ -1,29 +1,31 @@
-// const router = require('express').Router
+const router = require("express").Router();
+const FitnessPlan = require("../../models/FitnessPlan");
 
-// try {
-//   const dbGalleryData = await FitnessPlan.findAll({
-//     include: [
-//       {
-//         model: Workout,
-//         attributes: ["filename", "description"],
-//       },
-//     ],
-//   });
-//   const fitplans = dbFitnessPlanData.map((fitplan) =>
-//     fitplan.get({ plain: true })
-//   );
+// GET /api/fitnessplan
+// GET all fitness plans
+router.get("/", async (req, res) => {
+  try {
+    const dbFitnessData = await FitnessPlan.findAll();
+    if (dbFitnessData) res.status(200).json(dbFitnessData);
+    else res.status(404).json({ message: "Couldn't find user" });
+    // const fitplans = dbFitnessPlanData.map((fitplan) =>
+    //   fitplan.get({ plain: true })
+    // );
 
-//   req.session.save(() => {
-//     if (req.session.countVisit) {
-//       req.session.countVisit++;
-//     } else {
-//       req.session.countVisit = 1;
-//     }
-//     res.render("homepage", {
-//       fitplans,
-//       countVisit: req.session.countVisit,
-//     });
-//   });
-// } catch (err) {
-//   res.status(500).json(err);
-// }
+    req.session.save(() => {
+      if (req.session.countVisit) {
+        req.session.countVisit++;
+      } else {
+        req.session.countVisit = 1;
+      }
+      res.render("insert handlebars here", {
+        fitplans,
+        countVisit: req.session.countVisit,
+      });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
--- models/Recipe.js
+++ models/Recipe.js
@@ -3,31 +3,40 @@ const sequelize = require("../config/connection");
 
 class Recipe extends Model {}
 
-Recipe.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  recipe_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  recipe_url: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    validate: {
-      isUrl: true,
+Recipe.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    recipe_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    recipe_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true,
+      },
+    },
+    mealplan_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "mealplan",
+        key: "id",
+      },
     },
   },
-  mealplan_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: "mealplan",
-      key: "id",
-    },
-  },
-});
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "recipe",
+  }
+);
 
 module.exports = Recipe;
