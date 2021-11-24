d5973700d4b839061585f59a66098957da013749
Hugo Yanez <hugodavidyanez@gmail.com>
2021-11-24 11:54:49 -0500

    Created an PUT request to update a meal plan by id

--- controllers/api/meal-plan-routes.js
+++ controllers/api/meal-plan-routes.js
@@ -43,4 +43,36 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+// POST /api/mealplan
+// CREATE a new meal plan
+router.post("/", async (req, res) => {
+  try {
+    const mealData = await MealPlan.create(req.body);
+    res.status(200).json(mealData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// PUT /api/mealplan/:id
+// UPDATE a meal plan by id
+router.put("/:id", async (req, res) => {
+  try {
+    const mealData = await MealPlan.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!mealData) {
+      res.status(404).json({ message: "No Fitness Plan found with this id!" });
+      return;
+    }
+
+    res.status(200).json(mealData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
