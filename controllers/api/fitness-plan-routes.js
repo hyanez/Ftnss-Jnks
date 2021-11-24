fc7ac66d640d7d1598dea7daf0e183d1854845fe
Hugo Yanez <hugodavidyanez@gmail.com>
2021-11-24 11:14:42 -0500

    Created GET requests for fitness-plan route

--- controllers/api/fitness-plan-routes.js
+++ controllers/api/fitness-plan-routes.js
@@ -6,8 +6,7 @@ const FitnessPlan = require("../../models/FitnessPlan");
 router.get("/", async (req, res) => {
   try {
     const dbFitnessData = await FitnessPlan.findAll();
-    if (dbFitnessData) res.status(200).json(dbFitnessData);
-    else res.status(404).json({ message: "Fitness Plans don't exist" });
+    res.status(200).json(dbFitnessData);
     // const fitplans = dbFitnessPlanData.map((fitplan) =>
     //   fitplan.get({ plain: true })
     // );
@@ -42,9 +41,5 @@ router.get("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
-
-app.post("/", (req, res) => res.json(`POST route`));
-app.put("/:id", (req, res) => res.json(`PUT route`));
-app.delete("/:id", (req, res) => res.json(`DELETE route`));
 
 module.exports = router;
