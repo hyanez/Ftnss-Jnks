d08df02aa49aa6fbfb43a2c9d80bd4ac4a86ba81
Hugo Yanez <hugodavidyanez@gmail.com>
2021-11-24 11:44:20 -0500

    Created a GET request to get all meal plans

--- controllers/api/meal-plan-routes.js
+++ controllers/api/meal-plan-routes.js
@@ -5,24 +5,23 @@ const MealPlan = require("../../models/MealPlan");
 // GET all meal plans
 router.get("/", async (req, res) => {
   try {
-    const dbMealData = await MealPlan.findAll();
-    if (dbMealData) res.status(200).json(dbMealData);
-    else res.status(404).json({ message: "Couldn't find user" });
+    const mealData = await MealPlan.findAll();
+    res.status(200).json(mealData);
     // const fitplans = dbFitnessPlanData.map((fitplan) =>
     //   fitplan.get({ plain: true })
     // );
 
-    req.session.save(() => {
-      if (req.session.countVisit) {
-        req.session.countVisit++;
-      } else {
-        req.session.countVisit = 1;
-      }
-      res.render("insert handlebars here", {
-        fitplans,
-        countVisit: req.session.countVisit,
-      });
-    });
+    // req.session.save(() => {
+    //   if (req.session.countVisit) {
+    //     req.session.countVisit++;
+    //   } else {
+    //     req.session.countVisit = 1;
+    //   }
+    //   res.render("insert handlebars here", {
+    //     fitplans,
+    //     countVisit: req.session.countVisit,
+    //   });
+    // });
   } catch (err) {
     res.status(500).json(err);
   }
