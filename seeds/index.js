ae6cc4ba66844136413435868cd8eea2f3ce283d
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-24 10:38:57 -0500

    created recipe seed data

--- seeds/index.js
+++ seeds/index.js
@@ -6,6 +6,7 @@ const userSeedData = require("./userSeedData.json");
 const fitnessPlanSeedData = require("./fitnessPlanSeedData.json");
 const mealPlanSeedData = require("./mealPlanSeedData.json");
 const workoutSeedData = require("./workoutSeedData.json");
+const recipeSeedData = require("./recipeSeedData.json");
 
 const seedDatabase = async () => {};
 
