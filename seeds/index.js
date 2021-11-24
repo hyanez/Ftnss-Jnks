493e5b3a49ea1e25843ef92271c234778aeb56bd
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-24 09:22:26 -0500

    first run of seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,1 +1,10 @@
 //Seeds for database
+const sequelize = require("../config/connection");
+const { Traveller, Location, Trip } = require("../models");
+
+const travellerSeedData = require("./travellerSeedData.json");
+const locationSeedData = require("./locationSeedData.json");
+
+const seedDatabase = async () => {};
+
+seedDatabase();
