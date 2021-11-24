31ab43a23a0861c6f7ed2f851ffc9664d32d0b9a
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-24 09:47:04 -0500

    created seeds for fitnessplan data

--- seeds/index.js
+++ seeds/index.js
@@ -3,6 +3,7 @@ const sequelize = require("../config/connection");
 const { User } = require("../models");
 
 const userSeedData = require("./userSeedData.json");
+const fitnessPlanSeedData = require("./fitnessPlanSeedData.json");
 
 const seedDatabase = async () => {};
 
