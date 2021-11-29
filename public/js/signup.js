b59a7b35f0bdbf75776dece3a279e329fa1321a9
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-29 18:15:19 -0500

    fixed gender input in form

--- public/js/signup.js
+++ public/js/signup.js
@@ -6,10 +6,8 @@ const signupFormHandler = async (event) => {
   const height = document.querySelector("#height-signup").value.trim();
   const weight = document.querySelector("#weight-signup").value.trim();
   const age = document.querySelector("#age-signup").value.trim();
-  const gender = "male";
-
+  const gender = document.querySelector('input[name="gender"]:checked').value;
   if (email && password && height && weight && age && gender) {
-    console.log("three");
     const response = await fetch("/api/users", {
       method: "POST",
       body: JSON.stringify({ email, password, height, weight, age, gender }),
