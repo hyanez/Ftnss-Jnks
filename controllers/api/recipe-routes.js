0503bb2af7b37eb0c3899d8ea7d4477d8dde4083
whaberer <91747264+willhaberer@users.noreply.github.com>
2021-11-29 21:25:35 -0500

    added delete route for recipes

--- controllers/api/recipe-routes.js
+++ controllers/api/recipe-routes.js
@@ -132,4 +132,24 @@ router.post("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const recipeData = await Recipe.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!recipeData) {
+      res.status(404).json({ message: "No Recipe found with this id!" });
+      return;
+    }
+
+    res.status(200).json(recipeData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 module.exports = router;
